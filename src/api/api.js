39ef6c72c225134fde572ee30b785976002f9fdf
Jelte Homminga <jeltehomminga@gmail.com>
2019-05-07 11:52:58 +0200

    clean up API :lipstick:

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,31 @@
 import axios from "axios";
+const {
+  REACT_APP_API_METHOD_SEARCH: search,
+  REACT_APP_API_METHOD_RECENT: recent,
+  REACT_APP_API_KEY: key,
+  REACT_APP_API_URL: url
+} = process.env;
 
 const loadPhotos = (searchInput, page) => {
   //Thunk function returns a function
   return dispatch => {
-    axios({
-      method: "get",
-      //Search by tag if search input provided, otherwise call recent recommended by Flickr
-      //Safe search and sort by relevance to prevent NSFW pics, warning: NSFW can still show up!
-      url: `${process.env.REACT_APP_API_URL}?method=${
-        searchInput
-          ? process.env.REACT_APP_API_METHOD_SEARCH
-          : process.env.REACT_APP_API_METHOD_RECENT
-      }&api_key=${
-        process.env.REACT_APP_API_KEY
-      }&tags=${searchInput}&page=${page}&sort=relevance&safe_search=1&format=json&nojsoncallback=1`
-    }).then(response => {
-      dispatch({ type: "PHOTOS", photos: response.data.photos, page: page });
-    });
+    axios
+      .get(url, {
+        params: {
+          method: searchInput ? search : recent,
+          api_key: key,
+          tags: searchInput,
+          page: page,
+          sort: "relevance",
+          safe_search: "1",
+          format: "json",
+          nojsoncallback: 1
+        }
+      })
+      .then(response => {
+        dispatch({ type: "PHOTOS", photos: response.data.photos, page: page });
+      })
+      .catch(err => console.log(err));
   };
 };
 
